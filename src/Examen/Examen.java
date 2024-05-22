cd92a4e5546cd251f7ebbcf40f730ba05af1c1be
aleja <aleja@LAPTOP-IMPQVJ86>
2024-05-22 11:58:31 +0200

    Hola

---
+++ src/Examen/Examen.java
@@ -0,0 +1,19 @@
+package Examen;
+
+import java.util.Scanner;
+
+public class Examen {
+	
+	public static void main(String[]arg) {
+		
+
+		Scanner ent=new Scanner(System.in);
+		int numero=0;
+		System.out.println("Hola");
+		System.out.println("Dame un numero");
+		numero=Integer.parseInt(ent.nextLine());
+		
+		
+	}
+
+}
