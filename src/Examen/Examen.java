12b18ba0c777d605d5bdace9b08b0b307e34c683
aleja <aleja@LAPTOP-IMPQVJ86>
2024-05-22 12:06:46 +0200

    Hola

--- src/Examen/Examen.java
+++ src/Examen/Examen.java
@@ -12,8 +12,36 @@ public class Examen {
 		System.out.println("Hola");
 		System.out.println("Dame un numero");
 		numero=Integer.parseInt(ent.nextLine());
+		 if (esPrimo(numero)) {
+	            System.out.println(numero + " es un número primo.");
+	        } else {
+	            System.out.println(numero + " no es un número primo.");
+	        }
+	    }
+
+	    public static boolean esPrimo(int numero) {
+	        if (numero <= 1) {
+	            return false;
+	        }
+	        if (numero == 2) {
+	            return true;
+	        }
+	        if (numero % 2 == 0) {
+	            return false;
+	        }
+	        int limite = (int) Math.sqrt(numero);
+	        for (int i = 3; i <= limite; i += 2) {
+	            if (numero % i == 0) {
+	                return false;
+	            }
+	        }
+	        return true;
+	    }
+	
+			
+			
 		
 		
 	}
 
-}
+
